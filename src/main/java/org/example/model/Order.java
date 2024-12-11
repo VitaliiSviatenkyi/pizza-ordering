b2dff04c7745684df9ce8845794f3098c625f0c0
Vitalii Sviatenkyi <vexa2009@gmail.com>
2024-12-11 15:12:40 +0100

    Task-004: Add Database Integration Modify entities: Customer, Order, Pizza Added relations with entities

--- src/main/java/org/example/model/Order.java
+++ src/main/java/org/example/model/Order.java
@@ -4,19 +4,28 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
+@Table
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
     private Customer customer;
 
+    @OneToMany
+    @JoinColumn(name = "pizzaList_id")
     private List<Pizza> pizzaList;
 
     private LocalDateTime creationDate;
