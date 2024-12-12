56f81b4bd15be94e5e14cc15e21d0df966a6c682
Vitalii Sviatenkyi <vexa2009@gmail.com>
2024-12-12 13:52:54 +0100

    Task-007: Add API Endpoints for Pizzas #7 Modify Pizza.java Deleted PizzaName.java Added pizza examples to 0.init-schema.xml for database initialization

--- src/main/java/org/example/model/Pizza.java
+++ src/main/java/org/example/model/Pizza.java
@@ -5,47 +5,28 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import org.example.model.enums.PizzaName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table
+@Table(name = "pizzas")
 public class Pizza {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private PizzaName name;
+    private String name;
 
     private BigDecimal price;
 
-    public Pizza() {
-    }
 
-    public Pizza(PizzaName name, BigDecimal price) {
-        this.name = name;
-        this.price = price;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public PizzaName getName() {
-        return name;
-    }
-
-    public void setName(PizzaName name) {
-        this.name = name;
-    }
 }
