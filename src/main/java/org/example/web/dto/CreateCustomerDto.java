f8ae3141c78a07d116aecf3e16fb7d8a690188d3
Vitalii Sviatenkyi <vexa2009@gmail.com>
2024-12-12 11:36:50 +0100

    Task-003: Add API Endpoints for Customer #3 Modify 0.init-schema.xml (Allowed null values for the 'orderList_id' column in the Customer entity) Added Lombok annotations to Customer.java Added CreateCustomerDto.java Added CustomerDto.java (without the 'orderList' value)

---
+++ src/main/java/org/example/web/dto/CreateCustomerDto.java
@@ -0,0 +1,16 @@
+package org.example.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateCustomerDto {
+
+    private String name;
+    private String address;
+}
