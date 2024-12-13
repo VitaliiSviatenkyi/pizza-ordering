09a2f53cfcf76a8a84351098b3151840282ba7d3
Vitalii Sviatenkyi <vexa2009@gmail.com>
2024-12-13 10:23:42 +0100

    Task-014: Add CRUD for Pizza #14
    Added findById, update, and delete methods to the controller, facade, and service layers

--- src/main/java/org/example/facade/PizzaFacade.java
+++ src/main/java/org/example/facade/PizzaFacade.java
@@ -10,4 +10,10 @@ public interface PizzaFacade {
     PizzaDto save(CreatePizzaDto createPizzaDto);
 
     List<PizzaDto> findAll();
+
+    PizzaDto findById(Integer id);
+
+    PizzaDto update(PizzaDto pizzaDto);
+
+    void delete(Integer id);
 }
--- src/main/java/org/example/service/impl/PizzaServiceImpl.java
+++ src/main/java/org/example/service/impl/PizzaServiceImpl.java
@@ -1,6 +1,7 @@
 package org.example.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.example.exception.EntityNotFoundException;
 import org.example.model.Pizza;
 import org.example.repository.PizzaRepository;
 import org.example.service.PizzaService;
@@ -24,5 +25,24 @@ public class PizzaServiceImpl implements PizzaService {
     @Override
     public List<Pizza> findAll() {
         return pizzaRepository.findAll();
+    }
+
+    @Override
+    public Pizza findById(Integer id) {
+        return pizzaRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Pizza not found"));
+    }
+
+    @Override
+    public Pizza update(Pizza pizza) {
+        Pizza updatePizza = pizzaRepository.findById(pizza.getId())
+                .orElseThrow(() -> new EntityNotFoundException("Pizza not found"));
+        updatePizza.setName(pizza.getName());
+        updatePizza.setPrice(pizza.getPrice());
+        return pizzaRepository.save(updatePizza);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        pizzaRepository.deleteById(id);
     }
 }
