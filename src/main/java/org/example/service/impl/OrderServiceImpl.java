fa795e77b231849e58acb182f5a2f5ecafa269b3
Vitalii Sviatenkyi <vexa2009@gmail.com>
2024-12-13 16:43:09 +0100

    Task-012: Add CRUD for Order, service layer, facade layer #12
    Added Order facade layer and
    modify OrderController.java + service layer

--- src/main/java/org/example/service/impl/OrderServiceImpl.java
+++ src/main/java/org/example/service/impl/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package org.example.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.example.exception.EntityNotFoundException;
+import org.example.exception.ProductNotFoundException;
 import org.example.model.Customer;
 import org.example.model.Order;
 import org.example.model.Pizza;
@@ -13,7 +15,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Primary
 @Service
@@ -26,23 +27,47 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order save(Integer customerId, List<Integer> pizzaIds) {
-        Optional<Customer> customer = customerRepository.findById(customerId);
-        if (customer.isEmpty()) {
-            throw new RuntimeException("Customer not found");
+        if (pizzaIds == null || pizzaIds.isEmpty()) {
+            throw new ProductNotFoundException("Pizza IDs list must not be null or empty");
         }
+        Customer customer = customerRepository.findById(customerId)
+                .orElseThrow(() -> new EntityNotFoundException("Customer not found"));
 
         List<Pizza> pizzas = pizzaRepository.findAllById(pizzaIds);
         if (pizzas.isEmpty()) {
-            throw new RuntimeException("No pizzas found for the given IDs");
+            throw new EntityNotFoundException("No pizzas found for the given IDs");
         }
-
         Order order = Order.builder()
-                .customer(customer.get())
+                .customer(customer)
                 .pizzaList(pizzas)
                 .creationDate(LocalDateTime.now())
                 .build();
 
         return orderRepository.save(order);
+    }
 
+    @Override
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    @Override
+    public Order findById(Integer id) {
+        return orderRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Order not found"));
+    }
+
+    @Override
+    public Order update(Integer orderId, Integer customerId, List<Integer> pizzaIds) {
+        Order updateOrder = orderRepository.findById(orderId)
+                .orElseThrow(() -> new EntityNotFoundException("Order not found"));
+        updateOrder.setCustomer(customerRepository.findById(customerId)
+                .orElseThrow(() -> new EntityNotFoundException("Customer not found")));
+        updateOrder.setPizzaList(pizzaRepository.findAllById(pizzaIds));
+        return orderRepository.save(updateOrder);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        orderRepository.deleteById(id);
     }
 }
